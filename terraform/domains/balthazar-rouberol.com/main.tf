339cd22e5c799d102c8a49007b1cf2b871d1aa4e
Balthazar Rouberol <br@imap.cc>
2019-06-10 14:18:05 +0200

    Manage private subdomains redirection to RPI:

--- terraform/domains/balthazar-rouberol.com/main.tf
+++ terraform/domains/balthazar-rouberol.com/main.tf
@@ -17,4 +17,24 @@ resource "ovh_domain_zone_record" "subdomains" {
     target = "${var.root_domain}."
 
     count = "${length(var.subdomains)}"
+    depends_on = [ovh_domain_zone_record.root_domain]
+}
+
+resource "ovh_domain_zone_record" "rpi" {
+    zone = "${var.root_domain}"
+    subdomain = "${var.root_private_subdomain}"
+    fieldtype = "A"
+    ttl = "0"
+    target = "${var.rpi_local_ip}"
+}
+
+resource "ovh_domain_zone_record" "private_subdomains" {
+    zone = "${var.root_domain}"
+    subdomain = "${element(var.private_subdomains, count.index)}"
+    fieldtype = "CNAME"
+    ttl = "0"
+    target = "${var.root_private_subdomain}.${var.root_domain}."
+
+    count = "${length(var.private_subdomains)}"
+    depends_on = [ovh_domain_zone_record.rpi]
 }
--- terraform/domains/balthazar-rouberol.com/variables.tf
+++ terraform/domains/balthazar-rouberol.com/variables.tf
@@ -4,14 +4,30 @@ variable "root_domain" {
   default = "balthazar-rouberol.com"
 }
 
+variable "root_private_subdomain" {
+  default = "pi"
+}
+
 variable "subdomains" {
   description = "The balthazar-rouberol.com subdomains we CNAME to the main domain"
   type = list(string)
-  default = ["stats", "blog", "irc", "bank", "ip"]
+  default = ["stats", "blog", "irc", "ip"]
+}
+
+variable "private_subdomains" {
+  description = "The balthazar-rouberol.com subdomains we CNAME to the private domain"
+  type = list(string)
+  default = ["bank"]
 }
 
 variable "main_domain_ip" {
   description = "The IP of the VPS behind the balthazar-rouberol.com domain"
   type = string
   default = "51.158.66.138"
+}
+
+variable "rpi_local_ip" {
+  description = "local IP of the raspberry pi"
+  type = string
+  default = "192.168.0.30"
 }
