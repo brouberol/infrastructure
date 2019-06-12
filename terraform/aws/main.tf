5d136f92a27f272729e150c249e0c34d183780e3
Balthazar Rouberol <br@imap.cc>
2019-06-12 08:54:07 +0200

    Define an s3 bucket to hold the terraform state

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -22,3 +22,15 @@ resource "aws_s3_bucket" "balthazar-rouberol-blog" {
       mfa_delete = false
   }
 }
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket   = "${module.global_vars.tfstate_s3_bucket_name}"
+  provider = "aws.euwest"
+  acl      = "private"
+  region   = "${module.global_vars.blog_s3_bucket_region}"
+  force_destroy = true
+  versioning {
+      enabled = false
+      mfa_delete = false
+  }
+}
