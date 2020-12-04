de7cc982bc3aed4db7c6d4f4cfa8cc509682b0d5
Balthazar Rouberol <br@imap.cc>
2020-12-04 17:41:00 +0100

    Upgrade the scalewat tf provider and resources

--- terraform/scaleway/main.tf
+++ terraform/scaleway/main.tf
@@ -8,46 +8,57 @@ module "global_vars" {
 }
 
 provider "scaleway" {
-  region = "${module.global_vars.scaleway_region}"
+  region = "fr-par"
+  zone = "fr-par-1"
+  version = "~> 1.17"
 }
 
-resource "scaleway_ssh_key" "ssh_key" {
-    key = "${element(module.global_vars.ssh_keys, count.index)}"
+resource "scaleway_account_ssh_key" "ssh_key" {
+    name = "${element(split("== ", element(module.global_vars.ssh_keys, count.index)), 1)}"
+    public_key = "${element(module.global_vars.ssh_keys, count.index)}"
     count = "${length(module.global_vars.ssh_keys)}"
 }
 
-resource "scaleway_server" "gallifrey" {
-  name  = "${module.global_vars.gallifrey_scaleway_server_name}"
-  image = "${module.global_vars.gallifrey_scaleway_image_id}"
-  type  = "${module.global_vars.gallifrey_scaleway_server_type}"
-  enable_ipv6 = false
-  tags = []
-}
+resource "scaleway_instance_ip" "gallifrey_ip" {}
 
-resource "scaleway_ip" "gallifrey_ip" {
-  server = "${scaleway_server.gallifrey.id}"
-}
-
-resource "scaleway_ip_reverse_dns" "gallifrey" {
-  ip = "${scaleway_ip.gallifrey_ip.id}"
-  reverse = "${module.global_vars.root_domain}"
-}
-
-resource "scaleway_volume" "gallifrey_snap" {
+resource "scaleway_instance_volume" "gallifrey_snap" {
   name       = "snapshot_gallifrey_0_2019-06-30_17-29"
   size_in_gb = 25
   type       = "l_ssd"
 }
 
-resource "scaleway_volume" "gallifrey_data" {
+resource "scaleway_instance_volume" "gallifrey_data" {
   name       = "Volume-1"
   size_in_gb = 15
   type       = "l_ssd"
 }
 
-resource "scaleway_security_group" "default_sg" {
-  name = "Default security group"
-  description             = "Auto generated security group."
-  enable_default_security = false
+resource "scaleway_instance_server" "gallifrey" {
+  name  = "${module.global_vars.gallifrey_scaleway_server_name}"
+  image = "${module.global_vars.gallifrey_scaleway_image_id}"
+  type  = "${module.global_vars.gallifrey_scaleway_server_type}"
+  ip_id = "${scaleway_instance_ip.gallifrey_ip.id}"
+  enable_ipv6 = false
+  tags = []
+  additional_volume_ids = [scaleway_instance_volume.gallifrey_data.id]
+  security_group_id = scaleway_instance_security_group.default_sg.id
+
+  depends_on = [
+    scaleway_instance_ip.gallifrey_ip,
+    scaleway_instance_volume.gallifrey_snap,
+    scaleway_instance_volume.gallifrey_data,
+    scaleway_instance_security_group.default_sg
+  ]
+}
+
+resource "scaleway_instance_ip_reverse_dns" "gallifrey" {
+  ip_id = "${scaleway_instance_ip.gallifrey_ip.id}"
+  reverse = "${module.global_vars.root_domain}"
+
+  depends_on = [scaleway_instance_ip.gallifrey_ip]
+}
+
+resource "scaleway_instance_security_group" "default_sg" {
+  description = "Auto generated security group."
   stateful = false
 }
