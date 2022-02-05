60f9896e923945c69d0f17f2a56effb23e418870
Balthazar Rouberol <br@imap.cc>
2022-02-05 12:52:59 +0100

    Delete everything associated w/ gallifrey at scaleway

--- terraform/scaleway/main.tf
+++ terraform/scaleway/main.tf
@@ -25,45 +25,6 @@ resource "scaleway_account_ssh_key" "ssh_key" {
   count      = length(module.global_vars.ssh_keys)
 }
 
-resource "scaleway_instance_ip" "gallifrey_ip" {}
-
-resource "scaleway_instance_volume" "gallifrey_snap" {
-  name       = "snapshot_gallifrey_0_2019-06-30_17-29"
-  size_in_gb = 25
-  type       = "l_ssd"
-}
-
-resource "scaleway_instance_volume" "gallifrey_data" {
-  name       = "Volume-1"
-  size_in_gb = 15
-  type       = "l_ssd"
-}
-
-resource "scaleway_instance_server" "gallifrey" {
-  name                  = module.global_vars.gallifrey_scaleway_server_name
-  image                 = module.global_vars.gallifrey_scaleway_image_id
-  type                  = module.global_vars.gallifrey_scaleway_server_type
-  ip_id                 = scaleway_instance_ip.gallifrey_ip.id
-  enable_ipv6           = false
-  tags                  = []
-  additional_volume_ids = [scaleway_instance_volume.gallifrey_data.id]
-  security_group_id     = scaleway_instance_security_group.default_sg.id
-
-  depends_on = [
-    scaleway_instance_ip.gallifrey_ip,
-    scaleway_instance_volume.gallifrey_snap,
-    scaleway_instance_volume.gallifrey_data,
-    scaleway_instance_security_group.default_sg
-  ]
-}
-
-resource "scaleway_instance_ip_reverse_dns" "gallifrey" {
-  ip_id   = scaleway_instance_ip.gallifrey_ip.id
-  reverse = module.global_vars.root_domain
-
-  depends_on = [scaleway_instance_ip.gallifrey_ip]
-}
-
 resource "scaleway_instance_security_group" "default_sg" {
   description = "Auto generated security group."
   stateful    = false
