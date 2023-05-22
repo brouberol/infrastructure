4cdfca92961a9920ee8b47980b127dd15a1b1bee
Balthazar Rouberol <br@imap.cc>
2023-05-22 14:55:46 +0200

    Simplify env var extraction code

--- terraform/env.sh
+++ terraform/env.sh
@@ -2,12 +2,18 @@
 DOGRC=$HOME/.dogrc
 AWS_CREDS=$HOME/.aws/credentials
 
+function extract_var_from_file {
+    local varname=$1;
+    local filename=$2;
+    echo $(grep $varname $filename | cut -d= -f2 | sed 's/ //g')
+}
+
 if [ -e $DOGRC ]; then
-    export DATADOG_API_KEY=$(grep apikey $DOGRC | cut -d= -f2 | sed 's/ //g')
-    export DATADOG_APP_KEY=$(grep appkey $DOGRC | cut -d= -f2 | sed 's/ //g')
+    export DATADOG_API_KEY=$(extract_var_from_file apikey $DOGRC)
+    export DATADOG_APP_KEY=$(extract_var_from_file appkey $DOGRC)
 fi
 
 if [ -e $AWS_CREDS ]; then
-    export AWS_ACCESS_KEY_ID=$(grep aws_access_key_id $AWS_CREDS | cut -d= -f2 | sed 's/ //g')
-    export AWS_SECRET_ACCESS_KEY=$(grep aws_secret_access_key $AWS_CREDS | cut -d= -f2 | sed 's/ //g')
+    export AWS_ACCESS_KEY_ID=$(extract_var_from_file aws_access_key_id $AWS_CREDS)
+    export AWS_SECRET_ACCESS_KEY=$(extract_var_from_file aws_secret_access_key $AWS_CREDS)
 fi
