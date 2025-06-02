c946749ffe3824cceb4a66534af6be97074af73e
Balthazar Rouberol <br@imap.cc>
2025-06-02 19:12:35 +0200

    Allow the override of the feed title

--- ansible/roles/gallifrey/yarr/files/rss_filter.py
+++ ansible/roles/gallifrey/yarr/files/rss_filter.py
@@ -13,16 +13,10 @@ def add_entry_to_feed(feed, entry):
     fe.pubDate(entry.get('published'))
 
 
-def filter_feed(input_url, search_string, negative_filter, output_filename):
+def filter_feed(input_url, title, search_string, negative_filter, output_filename):
     """
     Filter an RSS feed to include only entries whose titles contain the specified substring.
 
-    Args:
-        input_url (str): URL of the input RSS feed
-        search_string (str): Substring to search for in entry titles
-
-    Returns:
-        str: Path to the filtered RSS feed file
     """
     # Get the source feed
     source_feed = feedparser.parse(input_url)
@@ -32,14 +26,15 @@ def filter_feed(input_url, search_string, negative_filter, output_filename):
     fg.id(input_url)
     fg.link(href=input_url)
     fg.description(f"Filtered feed from {input_url}")
-    fg.title(source_feed.feed.title)
-
-
+    if not title:
+        fg.title(source_feed.feed.title)
 
     # Add entries that match our criteria
     for entry in source_feed.entries:
-        if negative_filter and negative_filter.lower() not in entry.get('title', '').lower():
-            add_entry_to_feed(fg, entry)
+        if negative_filter:
+            nf = negative_filter.lower()
+            if nf not in entry.get('title', '').lower() and nf not in entry.get('description', '').lower():
+                add_entry_to_feed(fg, entry)
         else:
             for field in ('title', 'description'):
                 if search_string and search_string.lower() in entry.get(field, '').lower():
@@ -47,15 +42,18 @@ def filter_feed(input_url, search_string, negative_filter, output_filename):
 
     # Save the feed
     fg.rss_file(output_filename)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Filter an RSS feed based on title substring')
     parser.add_argument('--url', help='URL of the RSS feed to filter')
     parser.add_argument('--filter', help='Substring to search for in entry titles/descriptions')
     parser.add_argument('--negative-filter', help='Substring to exclude from entry titles')
     parser.add_argument('--output', help='Output filename')
+    parser.add_argument('--title', help="The feed title. If not provided, the RSS feed title will be used.")
     args = parser.parse_args()
 
-    filter_feed(args.url, args.filter, args.negative_filter, args.output)
+    filter_feed(args.url,args.title, args.filter, args.negative_filter, args.output)
 
 
 if __name__ == "__main__":
